f93f7a468885c09939f955bc35f907388cfb299c
Matheus Bernini <matheus.b.peron@gmail.com>
2018-04-11 18:02:37 -0300

    Segunda tentativa de colocar no GitHub

--- PyTools/__init__.py
+++ PyTools/__init__.py
@@ -1,4 +1,5 @@
 '''
+ A simple Python package with usefull (or not) tools. 
 '''
 
 import numpy as np
@@ -11,15 +12,35 @@ m_H = 1.6733e-24
 c = 2.99792458e10
 sigma_Thomsom = 6.65248e-25
 
-def Ledd_L0(mass_M0): #Return L_Eddington/L_Sun for a star, given its mass.
+def Ledd_L0(mass_M0): 
+	'''
+	Return L_Eddington/L_Sun for a star, given its mass.
+	INPUT: Integer or Float
+	OUTUP: Float
+	'''
 	return (M0/L0)*4*np.pi*G_*(mass_M0)*m_H*c/sigma_Thomsom
 
 def HelloWorld():
+	'''
+	Say Hello to this wonderful world!  
+	INPUT: None
+	OUTUP: String
+	'''
 	return "Hello World!"
 
 def Transpose_Table(tableName):
+	'''
+	Transpose a wild table using Pandas
+	INPUT: File
+	OUTUP: Pandas Dataframe
+	'''
 	df = pd.read_csv(tableName, comment = '#')
 	return df.T
 
 def Reverse_word(word):
+	'''
+	Put a word backwards
+	INPUT: String
+	OUTUP: String	
+	'''
 	return word.reverse()
